d4717ca9d65fc4288f654908734a7b13c63e235e
Grace Birgitta Hadhinata <146738334+gracebirgita@users.noreply.github.com>
2025-07-01 16:25:45 +0700

    Update dashboard.py delete nonfunctional code

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,18 +1,10 @@
 import streamlit as st
-from streamlit_extras.switch_page_button import switch_page
+# from streamlit_extras.switch_page_button import switch_page
 import time
 
 # st.write("# Dashboard")
 def dashboard():
-    # st.title("Dashboard page")
 
-    # # Konfigurasi Streamlit
-    # # st.set_page_config(page_title="Stock Prediction", layout="centered")
-    # st.markdown(
-    #     '<h3 style="color: gold;">Select dataset for prediction</h3>',
-    #     unsafe_allow_html=True,
-    # )
-    # Membaca query parameter untuk navigasi
    
     st.title("Welcome to the Stock Prediction Web")
     st.write("")
@@ -20,27 +12,10 @@ def dashboard():
 
     st.image("https://images.unsplash.com/photo-1651341050677-24dba59ce0fd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", caption="Empowering Your Investment Decisions", use_container_width=True)
 
-    
-    # Penjelasan manfaat aplikasi
-    # st.markdown("""
-    # ### 
-    # - 📊 **Data-Driven Insights**: Make informed investment decisions.
-    # - 🚀 **Growth Opportunities**: Identify potential high-growth stocks early.
-    # - 🔍 **Risk Management**: Minimize losses by predicting market trends accurately.
-    # - 💡 **Empowered Decisions**: Combine AI predict stocks.
-                
-
-    # """)
 
     st.write("")
     st.write("")
 
-    # Tombol Explore
-
-    # if st.button("🚀 Explore Stock Prediction"):
-    #     switch_page("predict")
-        # navigate_to("explore")
-    # Halaman Welcome
     st.markdown(
         '<h3 style="color: gold;">Stock</h3><br></br>',
         unsafe_allow_html=True,
@@ -64,14 +39,7 @@ def dashboard():
         """,
         unsafe_allow_html=True
     )
-    # logo_urls = [
-    #     "https://mainsaham.id/wp-content/uploads/2023/02/img-BBCA.png",
-    #     "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/BRI_2020.svg/2560px-BRI_2020.svg.png",
-    #     "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Bank_Mandiri_logo_2016.svg/2560px-Bank_Mandiri_logo_2016.svg.png",
-    # ]
-    # cols = st.columns(3)
-    # for col, url in zip(cols, logo_urls):
-    #     col.image(url, width=100)
+
 
     st.write("")
     st.write("")
@@ -122,8 +90,5 @@ def dashboard():
     # Tombol interaktif untuk melanjutkan ke halaman berikutnya
     st.markdown("---")
 
-    # st.markdown("""
-    # <hr style="border:1px solid gray;margin-top:30px;margin-bottom:20px;">
-    # """, unsafe_allow_html=True)
    
 dashboard()
